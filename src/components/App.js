ceffb51decdc216940b191fdef75eda27d903b20
pranay2508 <bhatnagarpranay8@gmail.com>
2023-09-03 19:03:58 +0530

    added functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,20 @@
-import React from "react";
+import React , {useState} from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+
+
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Pranay",
-      email: "1234@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Pranjal",
-      email: "pranjal@gmail.com",
-    },
-  ];
+  
+const [ contacts , setContacts] = useState([]);
+const addContactHandler=(contact) =>{
+  console.log(contact);
+}
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts}/>
     </div>
   );
